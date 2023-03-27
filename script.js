5f273b669270bbaf233173868fb118a0d230acd1
Justyna Mikuła-Adydan <127610648+JustynaMikula@users.noreply.github.com>
2023-03-27 22:57:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const tytulElement = document.getElementById("title");
+tytulElement.textContent = "Egzamin front end developer ";
+console.log(tytulElement);
+
+const podtytulElement = document.querySelector(".subtitle");
+podtytulElement.style.color = "green";
+podtytulElement.innerText = "poziom zaawansowany";
+console.log(podtytulElement);
+
+const imieEl = document.querySelector(".imieEl");
+imieEl.innerText = "Justyna";
+console.log(imieEl);
+
+const nazwiskoEl = document.querySelector(".nazwiskoEl");
+nazwiskoEl.innerText = "Mikuła-Adydan";
+console.log(nazwiskoEl);
+
+// Stwórz plik index.html oraz dołącz do niego zewnętrzne arkusze stylów oraz plik ze skryptami.
+// W pliku index.html stwórz prosty artykuł z wykorzystaniem znaczników nagłówkowych i akapitowych.
+// Ostyluj nagłówki i tekst w zewnętrznych arkuszach stylów. W pliku skryptowym użyj metody do wypisania na ekranie.
+// Wypisz za pomocą tej metody swoje imię i nazwisko.
+
